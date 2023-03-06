2581c0f2314cc2d5be070dec853c96bdb64cd85f
GabenZone <j2002eduardo@gmail.com>
2023-03-06 23:13:19 +0000

    Sonic Exe Stuff 2

--- mods/SonicEXE/stages/stage.lua
+++
@@ -1,8 +0,0 @@
-function onMoveCamera(focus)
-    if focus == 'boyfriend' then
-        setProperty('defaultCamZoom', 0.9)
-    else if focus == 'dad' then
-        setProperty('defaultCamZoom', 0.56)
-    end
-end
-end
--- mods/SonicEXE/stages/toos.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-
-        makeLuaSprite('slow', 'slow',3689, 673);
-	setScrollFactor('', 1.25, 1.25);
- 
-	addLuaSprite('slow', false);
-
-        setProperty('slow.visible', true);
-end
